ef5b51fc9633a5d4f54fb39fae31b47feb4f052d
Zyhuto <119459963+Zyhuto@users.noreply.github.com>
2024-08-07 12:59:40 +0200

    Add Grid Elements | Add Grid Resizing

---
+++ script.js
@@ -0,0 +1,31 @@
+const squareContainer = document.querySelector(".square-container");
+
+function createChildElement () {
+    let squareAmount = getComputedStyle(squareContainer).getPropertyValue("--side-length") ** 2
+    for(i = 0; i < squareAmount; i++) {
+        const newSquare = document.createElement("div");
+        newSquare.classList.add("square");
+        squareContainer.appendChild(newSquare);
+
+    }
+
+
+}
+
+
+function changeSize () {
+    let  newSize = +prompt("enter new Size Between 1-100");
+    if (!Number.isInteger(newSize) || !(newSize > 0) || newSize > 100){
+        alert("wrong Input")
+        return changeSize();}
+    squareContainer.innerHTML = "";
+    squareContainer.style.setProperty("--side-length", newSize);
+    createChildElement();
+    return;
+    
+
+}
+
+const changeSizeButton = document.querySelector(".size-button");
+
+changeSizeButton.addEventListener("click", e => changeSize());
